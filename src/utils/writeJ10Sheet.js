470ad86fabab70d2c9fc544b71817717c16d808c
Jordan Quan <x229102@l600566.lan>
2021-06-06 17:52:32 -0400

    feat: accomodate multiple J10 sheet versions

--- src/utils/writeJ10Sheet.js
+++ src/utils/writeJ10Sheet.js
@@ -5,33 +5,37 @@ import { zip, replaceData } from './'
 export const writeJ10Sheet = ({ sheetInfo, workbook, month, winery }) => {
   const { name, outputSheets } = sheetInfo
   const ws = workbook.Sheets[name]
-  const data = XLSX.utils.sheet_to_json(ws, { header: 1 })
+  let data = XLSX.utils.sheet_to_json(ws, { header: 1 })
 
-  const promises = outputSheets.map(({ name, categories, rows, columnNames, model }) => {
-    let content = data.slice(...rows)
+  const promises = outputSheets.map(
+    ({ name, header, categories, endHeader, columnNames, model }) => {
+      const start = data.slice(data.findIndex((row) => row[1] === header))
+      const totalIndex = start.findIndex((row) => row[1] === endHeader)
+      let content = start.slice(1, totalIndex)
+      data = start.slice(totalIndex)
 
-    if (name !== CONTAINER_DEPOSITS) {
-      content = content.map((row) => {
-        const values = row.filter((cell) => !!cell || cell === 0)
-        return { winery, month, ...zip(columnNames, values) }
-      })
-    } else {
-      const container = []
-
-      content.forEach((row, index) => {
-        const { label, end } = categories.find(({ end }) => index <= end || end === undefined)
-        if (index !== end) {
+      if (name !== CONTAINER_DEPOSITS) {
+        content = content.map((row) => {
           const values = row.filter((cell) => !!cell || cell === 0)
-          container.push({ winery, month, label, ...zip(columnNames, values) })
-        }
-      })
+          return { winery, month, ...zip(columnNames, values) }
+        })
+      } else {
+        const container = []
+        content.forEach((row, index) => {
+          const { label, end } = categories.find(({ end }) => index <= end || end === undefined)
+          if (index !== end && index !== 0 && row[1] !== 'Subtotal') {
+            const values = row.filter((cell) => !!cell || cell === 0)
+            container.push({ winery, month, label, ...zip(columnNames, values) })
+          }
+        })
 
-      content = container
+        content = container
+      }
+
+      // write sheet
+      return replaceData({ model, data: content, month, winery })
     }
-
-    // write sheet
-    return replaceData({ model, data: content, month, winery })
-  })
+  )
 
   return Promise.all(promises)
 }
